823da269e5b50768da0065183c8f36369d23db56
mnewtonten <mnewton@trinity.local>
2016-11-12 12:12:07 -0600

    Made a bat class and a way to generate bats
    BATSHIT

--- Assets/Scripts/BatAttack.cs
+++ Assets/Scripts/BatAttack.cs
@@ -3,17 +3,45 @@ using System.Collections;
 
 public class BatAttack : MonoBehaviour {
 
-    GameObject Bat;
+    public Bat bestTestBat;
+
+    public class Bat {
+        public
+        Bat(Vector3 startingLoc) { loc = startingLoc; }
+
+        Vector3 loc;
+        GameObject BatObj;
+
+
+
+        public void Generate() {
+            BatObj = Resources.Load<GameObject>("Prefabs/bestbat");
+            BatObj = (GameObject)Instantiate(BatObj, new Vector3(1, 1, 2), Quaternion.identity);
+        }
+
+        public void Jitter() {
+
+        }
+
+        public void Swoop() {
+            BatObj.transform.eulerAngles = new Vector3(0f, 0f, 30f);
+
+        }
+
+    }
 
 	// Use this for initialization
 	void Start () {
 
-        Bat = Resources.Load<GameObject>("Prefabs/bestbat");
+        bestTestBat = new Bat(new Vector3(1f, 1f, 1f));
+        bestTestBat.Generate();
+        bestTestBat.Swoop();
+
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+
+    }
 }
