eeb9dbe2a04bde99bcb3839911ac437031489c35
mnewtonten <mnewton@trinity.local>
2016-11-12 11:20:15 -0600

    Gun game
    there is now a gun in this game

---
+++ Assets/Scripts/BatAttack.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BatAttack : MonoBehaviour {
+
+    GameObject Bat;
+
+	// Use this for initialization
+	void Start () {
+
+        Bat = Resources.Load<GameObject>("Prefabs/bestbat");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
