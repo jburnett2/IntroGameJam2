60884c57c8633e51b22c82e5fdf4ccc5de7fa5e8
jburnett2 <jburnett@trinity.local>
2016-11-12 12:24:44 -0600

    player parts in place
    TOLD ME THE WORLD IS GONNA ROLL ME

---
+++ Assets/Scripts/playerMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Physics.IgnoreLayerCollision(8, 9, true); // 8 and 9 are layerID's for player and legs, true sets ignoring to true
+	}
+}
