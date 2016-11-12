99a54c9819a897ee39b3609e72ef219936e21149
mnewtonten <mnewton@trinity.local>
2016-11-12 16:15:44 -0600

    Saved scene, saved project

---
+++ Assets/Scripts/playerJump.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerJump : MonoBehaviour {
+    
+    public float jumpHeight;
+    private Rigidbody2D theRigidBody;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        theRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float inputY = Input.GetAxis("Vertical");
+
+        theRigidBody.AddForce(new Vector2(0, inputY * jumpHeight));
+    }
+}
