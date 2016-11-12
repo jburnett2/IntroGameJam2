0d14e9b162a06cd786a417de7ae42e5a65aa2e99
boakes <boakes1@trinity.edu>
2016-11-12 17:02:38 -0600

    AddedPart

--- Assets/Scripts/Scrolling.cs
+++ Assets/Scripts/Scrolling.cs
@@ -8,17 +8,17 @@ public class Scrolling : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		savedOffset = GetComponent<SpriteRenderer>().sharedMaterial.GetTextureOffset ("_MainTex");
+		savedOffset = GetComponent<Renderer>().sharedMaterial.GetTextureOffset ("_MainTex");
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float x = Mathf.Repeat (Time.time * scrollSpeed, 1);
 		Vector2 offset = new Vector2 (x, savedOffset.y);
-		GetComponent<SpriteRenderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex", offset);
 	}
 
 	void OnDisable(){
-		GetComponent<SpriteRenderer>().sharedMaterial.SetTextureOffset ("_MainTex",savedOffset);
+		GetComponent<Renderer>().sharedMaterial.SetTextureOffset ("_MainTex",savedOffset);
 	}
 }
