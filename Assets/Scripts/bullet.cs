8551493cc7a5fdded56288154530aee231da15e7
mnewtonten <mnewton@trinity.local>
2016-11-12 15:55:09 -0600

    Shooting and Gun Noise
    BANG BANG BANG BANG BANG BANG BANG BANG BANG BANG BANG BANG BANG BANG
    BANG BANG BANG BANG BANG BANG BANG BANG

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -3,13 +3,30 @@ using System.Collections;
 
 public class bullet : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    public Rigidbody2D theRigidBody;
+
+    // Use this for initialization
+    void Start () {
+        theRigidBody = GetComponent<Rigidbody2D>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        StartCoroutine(waitDestroy(2));
+
+    }
+
+    IEnumerator waitDestroy(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
--- Assets/Scripts/gunShoot.cs
+++ Assets/Scripts/gunShoot.cs
@@ -4,13 +4,23 @@ using System.Collections;
 public class gunShoot : MonoBehaviour {
 
     public float bulletSpeed;
+    public float kickback;
 
     public GameObject bulletObj;
     private bullet bScript;
+    private Transform barrel;
+    private Transform back;
+    private Rigidbody2D arm;
+    private AudioSource source;
+    
 
 	// Use this for initialization
 	void Start () {
         bScript = GetComponent<bullet>();
+        barrel = transform.GetChild(0);
+        back = transform.GetChild(1);
+        arm = GetComponentInParent<Rigidbody2D>();
+        source = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -18,7 +28,19 @@ public class gunShoot : MonoBehaviour {
         if (Input.GetButtonDown("Fire1"))
         {
             GameObject iBullet = (GameObject)Instantiate(bulletObj, transform.position, transform.rotation);
+            if (source.isPlaying)
+            {
+                source.Stop();
+                source.PlayOneShot(source.clip, 1f);
+            }
+            else
+            {
+                source.PlayOneShot(source.clip, 1f);
+            }
+            arm.AddForce(new Vector2((barrel.position.x - back.position.x) * kickback, (barrel.position.y - back.position.y) * kickback));
             Rigidbody2D rb = iBullet.GetComponent<Rigidbody2D>();
+            rb.AddForce(new Vector2((barrel.position.x - back.position.x)*bulletSpeed, (barrel.position.y - back.position.y)*bulletSpeed));
+            iBullet.transform.eulerAngles = new Vector3(0,0, transform.eulerAngles.z+90);
             //rb.velocity = bulletSpeed;
         }
 	}
