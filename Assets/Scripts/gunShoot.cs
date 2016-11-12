f10d8e523fbce1346868b5d30737a6fe6585d96e
jburnett2 <jburnett@trinity.local>
2016-11-12 15:07:40 -0600

    a hint of bullet
    just a sprinkle

--- Assets/Scripts/gunShoot.cs
+++ Assets/Scripts/gunShoot.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class gunShoot : MonoBehaviour {
 
+    public float bulletSpeed;
+
+    public GameObject bulletObj;
+    private bullet bScript;
+
 	// Use this for initialization
 	void Start () {
-	
+        bScript = GetComponent<bullet>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject iBullet = (GameObject)Instantiate(bulletObj, transform.position, transform.rotation);
+            Rigidbody2D rb = iBullet.GetComponent<Rigidbody2D>();
+            //rb.velocity = bulletSpeed;
+        }
 	}
 }
