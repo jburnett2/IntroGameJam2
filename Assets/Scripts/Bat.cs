672e479dafc246a83a479a6e0efc24160988c969
jburnett2 <jburnett@trinity.local>
2016-11-12 17:48:17 -0600

    BG music and shootin bats

--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class Bat : MonoBehaviour
 {
+    private Rigidbody2D theRigidBody;
 
+    void Start ()
+    {
+        theRigidBody = GetComponent<Rigidbody2D>();
+    }
 
     //Vector3 PivotLoc;
     public Sprite deadBat;
@@ -11,9 +16,29 @@ public class Bat : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("AAAAAH");
+        if (gameObject.transform.parent.gameObject.GetComponent<BatDeleter>().dead == false)
+        {
+            theRigidBody.velocity = new Vector2(0, 0);
+            theRigidBody.AddForce(new Vector2(0, 400));
+        }
         Destroy(gameObject.GetComponent<HingeJoint2D>());
         gameObject.GetComponent<SpriteRenderer>().sprite = deadBat;
         gameObject.transform.parent.gameObject.GetComponent<BatDeleter>().dead = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Bullet"))
+        {
+            if (gameObject.transform.parent.gameObject.GetComponent<BatDeleter>().dead == false)
+            {
+                theRigidBody.velocity = new Vector2(0, 0);
+                theRigidBody.AddForce(new Vector2(0, 400));
+            }
+            Destroy(gameObject.GetComponent<HingeJoint2D>());
+            gameObject.GetComponent<SpriteRenderer>().sprite = deadBat;
+            gameObject.transform.parent.gameObject.GetComponent<BatDeleter>().dead = true;
+        }
     }
     void FixedUpdate()
     {
---
+++ Assets/Scripts/musicManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class musicManager : MonoBehaviour {
+
+    private AudioSource source;
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+	}
+	
+    void Awake()
+    {
+        source.PlayOneShot(source.clip, .2f);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
